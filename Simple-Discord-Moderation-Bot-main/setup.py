79d8ba0a537e194f3ab0ef4d3a00e995d02ada7d
RARcodes <112138242+RARcodes@users.noreply.github.com>
2022-12-12 17:50:21 +0000

    Update setup.py

--- Simple-Discord-Moderation-Bot-main/setup.py
+++ Simple-Discord-Moderation-Bot-main/setup.py
@@ -7,7 +7,7 @@ w = Fore.WHITE
 
 print(f"""{r}
 ███╗   ███╗ █████╗ ██╗  ██╗███████╗██████╗\n████╗ ████║██╔══██╗██║ ██╔╝██╔════╝██╔══██╗\n██╔████╔██║███████║█████╔╝ █████╗  ██████╔╝\n██║╚██╔╝██║██╔══██║██╔═██╗ ██╔══╝  ██╔══██╗\n██║ ╚═╝ ██║██║  ██║██║  ██╗███████╗██║  ██║\n╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝      
-{r}-------------------------------------------\n{r}| {r}.rar#0001 {r}|{r}\n{r}-------------------------------------------\n
+{r}-------------------------------------------\n{r}| {r}.rar0001 {r}|{r} https://github.com/sumzum |{r}\n{r}-------------------------------------------\n
 {r}""")
 
 #config
@@ -16,26 +16,28 @@ SERVER = input("Input New Server name: ")
 CAT1 = "welcome"
 CHANNEL1 = "👋│joins"
 CHANNEL2 = "📘│rules"
-CAT2 = "info"
+CHANNEL3 = "✅│verify"
+CAT2 = "information"
 CHANNEL4 = "📢│announcements"
 CHANNEL5 = "🎉│giveaways"
 CHANNEL6 = "👥│feedback"
 CHANNEL7 = "🌐│links"
-CAT3 = "Main"
+CAT3 = "community"
 CHANNEL8 = "💬│chat"
 CHANNEL9 = "📁│help"
 CHANNEL10 = "🌼│off-topic"
 CHANNEL11 = "🔥│your-work"
 CAT4 = "other"
+CHANNEL12 = "💡│commands"
 CHANNEL13 = "📨│ticket"
 
 #prefix-settings
-bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
+bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
 
 #online-statement
 @bot.event
 async def on_ready():
-  print(f"\n{w}ready! │ !setup")
+  print(f"\n{w}ready! │ .setup")
 
 #script
 @bot.command()
@@ -49,6 +51,7 @@ async def setup(ctx):
   category = await guild.create_category(CAT1)
   await category.create_text_channel(CHANNEL1)
   await category.create_text_channel(CHANNEL2)
+  await category.create_text_channel(CHANNEL3)
   category = await guild.create_category(CAT2)
   await category.create_text_channel(CHANNEL4)
   await category.create_text_channel(CHANNEL5)
@@ -60,7 +63,10 @@ async def setup(ctx):
   await category.create_text_channel(CHANNEL10)
   await category.create_text_channel(CHANNEL11)
   category = await guild.create_category(CAT4)
+  await category.create_text_channel(CHANNEL12)
   await category.create_text_channel(CHANNEL13)
   
 #run-bot
 bot.run(TOKEN)
+
+#all credit to https://github.com/vyzv/discord-server-maker made this im just implementing it into my bot with some minor chnages to this code!!
